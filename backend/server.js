3827e4a2bc33d12414947740fa03c98f4197cce1
zulkarnainalia-droid <zulkarnain.ali.a@gmail.com>
2025-12-09 17:52:22 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from "express";
 const app = express();
 
-app.get("/api/users", (req, res) => {
-  res.json([
-    { id: 1, name: "Admin" },
-    { id: 2, name: "User" }
-  ]);
+app.get("/", (req, res) => {
+  res.json({ message: "Backend OK" });
+});
+
 
 // WAJIB!
 export default app;
