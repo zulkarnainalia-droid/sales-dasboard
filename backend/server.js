2bce906f2a63a72f6f2861ae923f3ae52c6cd99b
zulkarnainalia-droid <zulkarnain.ali.a@gmail.com>
2025-12-09 18:05:25 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
