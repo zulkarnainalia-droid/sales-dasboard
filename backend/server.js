43a6fcedd7c638f555c3def88967c0e6b7f40a3c
zulkarnainalia-droid <zulkarnain.ali.a@gmail.com>
2025-12-09 12:28:24 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,9 @@
-import express from "express";
-import cors from "cors";
-import salesRoutes from "./src/routes/sales.routes.js";
-import customerRoutes from "./src/routes/customer.routes.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "API is running in Vercel!" });
+  res.json({ status: "OK" });
 });
 
-// WAJIB: Export app sebagai handler untuk serverless
+// WAJIB!
 module.exports = app;
