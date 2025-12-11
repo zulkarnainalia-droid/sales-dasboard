1531f0d4d9bc88484613f5df281509e3598cbd47
zulkarnainalia-droid <zulkarnain.ali.a@gmail.com>
2025-12-11 11:31:20 +0700

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,15 @@
+import express from "express";
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
+app.use(express.json());
+
 module.exports = (req, res) => {
   res.status(200).json({ status: "OK" });
 };
+
+// WAJIB!
+module.exports = app;
